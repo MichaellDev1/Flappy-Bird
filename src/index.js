b1b35f30064abec5703b07ce892c7cd730e9926a
MichaellDev1 <santuchomichael131@gmail.com>
2023-12-03 17:31:23 -0300

    add class player

--- src/index.js
+++ src/index.js
@@ -4,5 +4,42 @@ var width = 800;
 var height = 400;
 canvas.width = width;
 canvas.height = height;
-//x, y, w, h
-ctx.fillRect(0, 0, width, height);
+//rectFill = (x,y,w,h)
+var Player = /** @class */ (function () {
+    function Player(_a) {
+        var _b = _a.width, width = _b === void 0 ? 64 : _b, _c = _a.height, height = _c === void 0 ? 64 : _c, position = _a.position;
+        this._height = 0;
+        this._width = 0;
+        this.position = {
+            y: 0,
+            x: 0
+        };
+        this._width = width;
+        this._height = height;
+        this.position = position;
+    }
+    Player.prototype.drag = function () {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, this._width, this._height);
+    };
+    Player.prototype.update = function () {
+        if (this.position.y + this._height < canvas.height) {
+            this.position.y++;
+        }
+        this.drag();
+    };
+    return Player;
+}());
+var player = new Player({
+    position: {
+        x: 30,
+        y: 130
+    }
+});
+function animation() {
+    window.requestAnimationFrame(animation);
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, width, height);
+    player.update();
+}
+animation();
--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,56 @@ let height = 400
 canvas.width = width
 canvas.height = height
 
-//x, y, w, h
-ctx.fillRect(0, 0, width, height)
+//rectFill = (x,y,w,h)
+
+class Player {
+    private readonly _height: number = 0
+    private readonly _width: number = 0
+    public position: {
+        y: number,
+        x: number
+    } = {
+            y: 0,
+            x: 0
+        }
+
+    constructor({ width = 64, height = 64, position }: {
+        width?: number, height?: number, position: {
+            y: number,
+            x: number
+        }
+    }) {
+        this._width = width
+        this._height = height
+        this.position = position
+    }
+
+    drag() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this._width, this._height)
+    }
+
+    update() {
+        if (this.position.y + this._height < canvas.height) {
+            this.position.y++
+        }
+        this.drag()
+    }
+}
+
+const player = new Player({
+    position: {
+        x: 30,
+        y: 130
+    }
+})
+
+function animation() {
+    window.requestAnimationFrame(animation)
+    ctx.fillStyle = '#fff'
+    ctx.fillRect(0, 0, width, height)
+
+    player.update()
+}
+
+animation()
